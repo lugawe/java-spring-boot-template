1a26249b30c5984125cfd226a171464801619c2c
Luis Weich <luis.gabriel.weich@gmail.com>
2025-10-19 18:22:04 +0200

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# java-spring-boot-template
+
+## Build and run
+
+Build jar with maven:
+
+```bash
+./mvnw clean package
+```
+
+Run jar with java:
+
+```bash
+java -jar target/java-spring-boot-template-1.0.jar
+```
+
+## Run tests
+
+Run tests
+
+```bash
+./mvnw test
+```
---
+++ src/test/java/template/AppTest.java
@@ -0,0 +1,15 @@
+package template;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+public class AppTest {
+
+    @Test
+    public void test() {
+        Assertions.assertTrue(true);
+    }
+}
